2839140485c30baf948adc1cddbf680b4a96e43c
Pasindu <163386074+pasindupiumal03@users.noreply.github.com>
2025-05-18 16:46:21 +0530

    update config

--- v1/vite.config.js
+++ v1/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
-    open: true,
+    historyApiFallback: true,
   },
 });
